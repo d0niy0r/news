ad69b92e8ee809f4eebcaa1fad51eb277e8fc2e6
doniyorbek <islomjonov.doniyorbek@gmail.com>
2023-10-17 12:19:24 +0500

    smal fixed

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -8,103 +8,110 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      DateFormat("EEEE, d MMMM").format(DateTime.now()),
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.normal,
+      body: InkWell(
+        onTap: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => NewsPage()));
+        },
+        child: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        DateFormat("EEEE, d MMMM").format(DateTime.now()),
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.normal,
+                        ),
                       ),
+                      Text(
+                        "Breaking News",
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 100),
+                  IconButton(
+                    icon: Icon(
+                      Icons.settings,
+                      color: Colors.black,
                     ),
-                    Text(
-                      "Breaking News",
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                    onPressed: () {
+                      // Navigator.of(context).push(
+                      //     MaterialPageRoute(builder: (context) => NewsPage()));
+                    },
+                  )
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Card(
+                      elevation: 32,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "CNN",
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    "Biden offers message for KIm Jong Un as he prepares to wrap first leg of his Asia trip-Cnn",
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    DateFormat("MMM d, y")
+                                        .format(DateTime.now()),
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w300,
+                                      color: Colors.black.withOpacity(0.4),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              height: 150,
+                              width: 150,
+                              margin: EdgeInsets.all(10),
+                              clipBehavior: Clip.hardEdge,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Image.asset(
+                                "assets/image/1.jpg",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
-                SizedBox(width: 100),
-                IconButton(
-                  icon: Icon(
-                    Icons.settings,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => NewsPage()));
-                  },
-                )
-              ],
-            ),
-            Expanded(
-              child: ListView(
-                children: [
-                  Card(
-                    elevation: 32,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "CNN",
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  "Biden offers message for KIm Jong Un as he prepares to wrap first leg of his Asia trip-Cnn",
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  DateFormat("MMM d, y").format(DateTime.now()),
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w300,
-                                    color: Colors.black.withOpacity(0.4),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: 150,
-                            width: 150,
-                            margin: EdgeInsets.all(10),
-                            clipBehavior: Clip.hardEdge,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Image.asset(
-                              "assets/image/1.jpg",
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
